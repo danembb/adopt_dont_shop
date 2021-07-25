fe9478ad44c57bb209335570d25aafb8739d1885
danembb <dbrophy720@gmail.com>
2021-07-25 13:18:30 -0600

    Configure routes file and create view files for tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/pets', to: '/pets#index'
+
+  get '/shelters', to: '/shelters#index'
+
+  get '/veterinarians', to: '/veterinarians#index'
+
+  get '/veterinary_offices', to: '/veterinary_offices#index'
 end
